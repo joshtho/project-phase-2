67f663d6408307621d6dfbd3d7e753715fdcf2fe
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-08-08 14:56:23 -0500

    update Navbar and make extra buttons

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -4,8 +4,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,20 +17,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ButtonAppBar() {
+export default function NavBar() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+          <Button color="inherit">Hyrule Monsters</Button>
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">Monster List</Button>
+          <Button color="inherit">Favorite Monster's List</Button>
         </Toolbar>
       </AppBar>
     </div>
