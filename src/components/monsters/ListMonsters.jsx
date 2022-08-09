291ff5e8e2a7abdf1da1133dca11887bdbd89d2c
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-08-09 14:50:34 -0500

    sorted monster list

--- src/components/monsters/ListMonsters.jsx
+++ src/components/monsters/ListMonsters.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import MonsterCard from './MonsterCard'
 
 const ListMonsters = ({monsters}) => {
-  const displayMonsters = monsters.map(monster => (
-    <MonsterCard monster={monster} />
+  const sortedMonsters = monsters.sort(function(a, b){
+    return a.id - b.id
+  });
+
+  const displayMonsters = sortedMonsters.map(monster => (
+    <MonsterCard key={monster.id} monster={monster} />
   ))
   
   return (
