d3fb2fa69f7c30ace9bb5dede54c70501fffb6c2
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-08-13 16:16:06 -0500

    create newCommentData state in CommentForm and write handleChange

--- src/components/comments/CommentForm.jsx
+++ src/components/comments/CommentForm.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
+import { useEffect } from 'react'
 
-const CommentForm = () => {
-const [username, setUsername] =  useState('')
-const [newComment, setNewComment] =  useState('')
-    
+const CommentForm = ({monsterName}) => {
+
+const [newCommentData, setNewCommentData] =  useState({
+    name: monsterName
+    username: '',
+    comment: ''
+})
+
+function handleChange(e) {
+    setNewCommentData({...newCommentData, [e.target.name]: e.target.value})
+}
+
 return (
-    <form>
+    <form onSubmit={}>
         <h3>Write comment here</h3>
         <label>Username:</label>
         <br/>
-        <input type='text' name="username" value={username} onChange={(e) => console.log(e.target.value)} />
+        <input type='text' name="username" value={username} onChange={handleChange} />
         <br/> 
         <label >Comment:</label>
         <br/>
-        <textarea type='text' name='comment' value={newComment} onChange={(e) => e.target.value} />
+        <textarea type='text' name='comment' value={newComment} onChange={handleChange} />
         <br/>
         <button>Submit</button>
     </form>
