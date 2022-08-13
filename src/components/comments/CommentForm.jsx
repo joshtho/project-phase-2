fee59df3805acde5cb70e53ea78bb00814e3a976
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-08-13 15:56:25 -0500

    get comments to display on monsterPage

--- src/components/comments/CommentForm.jsx
+++ src/components/comments/CommentForm.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CommentForm = () => {
-
-    return (
-        <form>
+const [username, setUsername] =  useState('')
+const [newComment, setNewComment] =  useState('')
+    
+return (
+    <form>
         <h3>Write comment here</h3>
         <label>Username:</label>
         <br/>
-        <input type='text' />
+        <input type='text' name="username" value={username} onChange={(e) => console.log(e.target.value)} />
         <br/> 
         <label >Comment:</label>
         <br/>
-        <textarea type='text' />
+        <textarea type='text' name='comment' value={newComment} onChange={(e) => e.target.value} />
         <br/>
         <button>Submit</button>
     </form>
--- src/components/comments/DisplayComments.jsx
+++ src/components/comments/DisplayComments.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 
 const DisplayComments = ({clickedMonster}) => {
-console.log(clickedMonster)
+const [comments, setComments] = useState([])
     
     useEffect(() => {
         fetch('http://localhost:3001/comments')
         .then(r => r.json())
-        .then(data => console.log(data.filter(monster => monster.name === clickedMonster)))
+        .then(data => setComments(data.filter(monster => monster.name === clickedMonster)))
     }, [])
     
+    const renderComment = comments.map(comment => (
+        <li key={comment.id}>{comment.username}: {comment.comment}</li>
+    ))
+
     return (
-    <div>Heres where the comments will go!</div>
+    <div>
+        <h3>Comments:</h3>
+        <ul>
+        {renderComment}
+        </ul>
+    </div>
   )
 }
 
