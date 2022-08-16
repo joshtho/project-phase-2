2229ed4d6e3756abab78e54b83ee29548d13b904
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-08-16 18:47:20 -0500

    figure out error, it didnt like common_locations array being named as variable, so moved same logic inside brackets in jsx and it worked

--- src/components/monsters/MonsterPage.jsx
+++ src/components/monsters/MonsterPage.jsx
@@ -8,7 +8,8 @@ const MonsterPage = ({monsters, capitalizeFirstLetter}) => {
   const [comments, setComments] = useState([])
   const params = useParams()
   const monsterObj = monsters.find(monster => monster.id === parseInt(params.id)) 
-
+console.log(monsters)
+console.log(params)
       console.log("Object",monsterObj)
   useEffect(() => {
         fetch('http://localhost:3001/comments')
@@ -17,29 +18,37 @@ const MonsterPage = ({monsters, capitalizeFirstLetter}) => {
     }, [])
   
   
-  const displayLocations = monsterObj.common_locations ? monsterObj.common_locations.map(location => (
-    <li key={location}>{location}</li> 
-  )) : "No common locations"
+  const displayLocations = () => (
+    monsterObj.common_locations ? monsterObj.common_locations.map(location => (
+      <li key={location}>{location}</li> 
+    )) : "Location varies"
+ 
+
+  )
+
+  // const displayDrops = monsterObj.drops ? monsterObj.drops.map(drop => (
+  //   <li key={drop}>{drop}</li>
+  // )) : "No common drops"
   
-
-  const displayDrops = monsterObj.drops ? monsterObj.drops.map(drop => (
-    <li key={drop}>{drop}</li>
-  )) : "No common drops"
-
+  
   return (
     <div className='card'>
 
       {monsterObj ? 
       <div>
-        <h1>Monster page</h1>
-        <h1>{capitalizeFirstLetter(monsterObj.name)}</h1>
+        <h2>{capitalizeFirstLetter(monsterObj.name)}</h2>
         <img alt={monsterObj.name} src={monsterObj.image} />
         <h4>{monsterObj.description}</h4>
         <ul>
         <h5>Common Locations</h5>
-          {displayLocations}
+        {displayLocations()}
+        {/* {monsterObj.common_locations ? monsterObj.common_locations.map(location => (
+          <li key={location}>{location}</li>
+        )) : "Location varies" } */}
         <h5>Drops</h5>
-        {displayDrops}
+        {/* {monsterObj.drops ? monsterObj.drops.map(drop => (
+          <li key={drop}>{drop}</li>
+        )) : "No Common drops"} */}
         </ul>
         <DisplayComments clickedMonster={monsterObj.name} comments={comments} />
         <CommentForm 
@@ -47,8 +56,9 @@ const MonsterPage = ({monsters, capitalizeFirstLetter}) => {
         setComments={setComments}
         comments={comments} 
         />
-      </div>
-        : <h1>Loading</h1>}
+      </div> : 
+      <h1>Loading...</h1>
+      }
 
     </div>
   )
