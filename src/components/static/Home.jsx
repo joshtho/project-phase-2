855fa250cdc8527aecf0bbfd8d7cde377e837d98
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-09-06 13:49:15 -0500

    add another header at Home

--- src/components/static/Home.jsx
+++ src/components/static/Home.jsx
@@ -5,15 +5,19 @@ import Container from '@material-ui/core/Container';
 const Home = () => {
   return (
     <div >
-      <h1>The Legend of Zelda: Breath of the Wild Beastiary</h1>
       <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-      <img alt='battle' className='center' src='https://www.ladbible.com/cdn-cgi/image/width=720,quality=70,format=jpeg,fit=pad,dpr=1/https%3A%2F%2Fs3-images.ladbible.com%2Fs3%2Fcontent%2F9b77bed85fc989284e65844d91f9d62a.png' />
-      </Container>
-    </React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm">
+      <h1>The Legend of Zelda: Breath of the Wild Bestiary</h1>
+      <h2>It's dangerous to go alone! Take this...</h2>
+          <img alt='battle' className='center' src='https://www.ladbible.com/cdn-cgi/image/width=720,quality=70,format=jpeg,fit=pad,dpr=1/https%3A%2F%2Fs3-images.ladbible.com%2Fs3%2Fcontent%2F9b77bed85fc989284e65844d91f9d62a.png' />
+        </Container>
+      </React.Fragment>
+      
     </div>
   )
 }
+
+
 
 export default Home    
